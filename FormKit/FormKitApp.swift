1fd37c16694c9e7374641c146812cdd9006e8c90
LachPawel <pawelkowalewsk@gmail.com>
2025-12-23 18:52:20 +0100

    Initial Commit

---
+++ FormKit/FormKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormKitApp.swift
+//  FormKit
+//
+//  Created by Pawel Kowalewski on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
